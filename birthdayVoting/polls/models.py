16d54629695180dd11421bf6bc9a114652b47a58
Ekaterina Kelembet <katyakelembet@gmail.com>
2017-04-28 10:18:33 +0300

    Remover field notes from Choices model, added mailgun service to the settings

--- birthdayVoting/polls/models.py
+++ birthdayVoting/polls/models.py
@@ -4,11 +4,10 @@ from polls.choices import *
 
 
 class Notes(models.Model):
-    notes_field = models.TextField('notes field', max_length=3000)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    notes_field = models.TextField('notes field', max_length=3000)
 
 
 class Choices(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     choice_fields = models.IntegerField(choices=CHOICES, default=1)
-    notes = models.OneToOneField(Notes, related_name='choices', blank=True, null=True)

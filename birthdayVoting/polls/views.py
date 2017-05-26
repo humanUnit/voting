631bfcc8de4683d9d63a3b641d079ee126e79656
Ekaterina Kelembet <katyakelembet@gmail.com>
2017-05-26 16:44:29 +0300

    Added new page notes made, also changed logic in the views

--- birthdayVoting/polls/views.py
+++ birthdayVoting/polls/views.py
@@ -31,11 +31,35 @@ def get_voting(request):
     if request.method == 'POST':
         form = BirthdayVoteForm(request.POST)
         if form.is_valid():
-            vote = form.save(commit=False)
-            user = request.user
-            vote.user = user
-            vote.save()
-            return HttpResponseRedirect(reverse('polls:notes'))
+            if Notes.objects.filter(user=request.user).exists():
+                vote = form.save(commit=False)
+                user = request.user
+                vote.user = user
+                vote.save()
+                context = {
+                    'contact_name': user.username,
+                    'contact_email': user.email,
+                    'choice': Choices.objects.get(user=user).get_choice_fields_display(),
+                    'notes': Notes.objects.get(user=user).notes_field if Notes.objects.filter(
+                        user=user).exists() else 'User left field empty',
+                }
+                template = get_template('contact_template')
+                content = template.render(context, request)
+                email = EmailMessage(
+                    "Someone's birthday soon",
+                    content,
+                    "Birthday voting" + '',
+                    to=['katyakelembet@gmail.com'],
+                    headers={'Reply-To': 'katyakelembet@gmail.com'}
+                )
+                email.send()
+                return HttpResponseRedirect(reverse('polls:thank_you'))
+            if not Notes.objects.filter(user=request.user).exists():
+                vote = form.save(commit=False)
+                user = request.user
+                vote.user = user
+                vote.save()
+                return HttpResponseRedirect(reverse('polls:notes'))
     else:
         form = BirthdayVoteForm()
     return render(request, 'voting_form.html', {'form': form})
@@ -55,38 +79,51 @@ def get_choices(request):
 @login_required
 def get_notes(request):
     if Notes.objects.filter(user=request.user).exists():
-        return HttpResponseRedirect(reverse('polls:thank_you'))
-    if not Choices.objects.filter(user=request.user).exists():
-        return HttpResponseRedirect(reverse('polls:voting'))
+        return HttpResponseRedirect(reverse('polls:notes_made'))
     if request.method == 'POST':
         form = BirthdayNoteForm(request.POST)
         if form.is_valid():
-            notes = form.save(commit=False)
-            user = request.user
-            notes.user = user
-            notes.save()
-            template = get_template('contact_template')
-            user = request.user
-            context = {
-                'contact_name': user.username,
-                'contact_email': user.email,
-                'choice': Choices.objects.get(user=user).get_choice_fields_display(),
-                'notes': Notes.objects.get(user=user).notes_field if Notes.objects.filter(
-                    user=user).exists() else 'User left field empty',
-            }
-            content = template.render(context, request)
-            email = EmailMessage(
-                "Someone's birthday soon",
-                content,
-                "Birthday voting" + '',
-                to=['katyakelembet@gmail.com'],
-                headers={'Reply-To': 'katyakelembet@gmail.com'}
-            )
-            email.send()
-            return HttpResponseRedirect(reverse('polls:thank_you'))
+            if not Choices.objects.filter(user=request.user).exists():
+                notes = form.save(commit=False)
+                user = request.user
+                notes.user = user
+                notes.save()
+                return HttpResponseRedirect(reverse('polls:voting'))
+            if Choices.objects.filter(user=request.user).exists():
+                notes = form.save(commit=False)
+                user = request.user
+                notes.user = user
+                notes.save()
+                template = get_template('contact_template')
+                user = request.user
+                context = {
+                    'contact_name': user.username,
+                    'contact_email': user.email,
+                    'choice': Choices.objects.get(user=user).get_choice_fields_display(),
+                    'notes': Notes.objects.get(user=user).notes_field if Notes.objects.filter(
+                        user=user).exists() else 'User left field empty',
+                }
+                content = template.render(context, request)
+                email = EmailMessage(
+                    "Someone's birthday soon",
+                    content,
+                    "Birthday voting" + '',
+                    to=['katyakelembet@gmail.com'],
+                    headers={'Reply-To': 'katyakelembet@gmail.com'}
+                )
+                email.send()
+                return HttpResponseRedirect(reverse('polls:thank_you'))
     else:
         form = BirthdayNoteForm()
     return render(request, 'notes_form.html', {'form': form})
+
+
+@login_required
+def get_note(request):
+    if request.method == 'POST':
+        return HttpResponseRedirect(reverse('polls:thank_you'))
+    else:
+        return render(request, 'notes_made.html')
 
 
 @login_required
